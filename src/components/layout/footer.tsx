ace5ab12863651b57aaf0d5cd334dd1a89eb0a0d
John Yorke <yorkejohn02@gmail.com>
2026-07-19 00:16:15 -0300

    Activity feed style changes, add year to copyright in footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="px-4 container mx-auto mt-12 md:mt-16">
       <div className="py-4 border-t text-sm font-mono text-muted text-center">
-        &copy; John Yorke / yorkejohn.dev / {process.env.VERSION}
+        &copy; {new Date().getFullYear()} John Yorke / yorkejohn.dev / {process.env.VERSION}
       </div>
     </footer>
   )
--- src/components/pages/home/activity-feed.tsx
+++ src/components/pages/home/activity-feed.tsx
@@ -3,7 +3,7 @@
 import {ArrowUpRightIcon} from '@phosphor-icons/react'
 import {formatDistanceToNow} from 'date-fns'
 import {use} from 'react'
-import {Anchor, Badge} from '@/components'
+import {Anchor} from '@/components'
 import {activity} from '@/lib/activity'
 
 export function ActivityFeed() {
@@ -14,12 +14,10 @@ export function ActivityFeed() {
       <Anchor
         href={compareUrl}
         key={index}
-        className="group interact:bg-accent-foreground flex flex-col md:flex-row gap-2 md:gap-8 py-2 cursor-pointer"
+        className="group interact:bg-accent-foreground flex flex-col md:flex-row gap-2 items-center py-2 cursor-pointer"
       >
-        <div>
-          <Badge className="text-accent-foreground group-interact:text-background w-40" variant="outline">
-            {formatDistanceToNow(pushedAt, {addSuffix: true})}
-          </Badge>
+        <div className="text-accent-foreground group-interact:text-background w-[22ch] text-xs">
+          {formatDistanceToNow(pushedAt, {addSuffix: true})}
         </div>
         <div className="group-interact:text-background text-nowrap">
           Pushed to
